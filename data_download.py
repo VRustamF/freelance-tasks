98bd7e3df3c768a255f7020d9c73b8504cc4da1a
VRustamF <118049337+VRustamF@users.noreply.github.com>
2024-11-19 08:51:57 +0700

    Update data_download.py

--- data_download.py
+++ data_download.py
@@ -2,11 +2,18 @@ import yfinance as yf
 
 
 def fetch_stock_data(ticker, period='1mo'):
+    """
+    Получает исторические данные об акциях для указанного тикера и временного периода.
+    Возвращает DataFrame с данными.
+    """
     stock = yf.Ticker(ticker)
     data = stock.history(period=period)
     return data
 
 
 def add_moving_average(data, window_size=5):
+    """
+    Добавляет в DataFrame колонку со скользящим средним, рассчитанным на основе цен закрытия.
+    """
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
